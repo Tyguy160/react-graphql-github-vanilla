4e4eb74ff170bc27a302be7c8c7fba9d3aba2655
EC2 Default User <ec2-user@ip-172-31-1-163.us-east-2.compute.internal>
2018-12-15 15:05:08 +0000

    Removed environmental variable

--- src/App.js
+++ src/App.js
@@ -13,11 +13,62 @@ const axiosGitHubGraphQL = axios.create({
 
 const TITLE = 'React GraphQL GitHub Client'
 
+const GET_ORGANIZATION = `
+{
+  organization(long: "the-road-to-learn-react") {
+    name
+    url
+  }
+}
+`
+
 class App extends Component {
+  
+  state = {
+    path: 'the-road-to-learn-react/the-road-to-learn-react',
+  }
+  
+  componentDidMount() {
+    // fetch data here
+    this.onFetchFromGitHub();
+  }
+  
+  onChange = e => {
+    this.setState({
+      path: e.target.value
+    })
+  }
+  
+  onSubmit = e => {
+    // fetch data
+    e.preventDefault();
+    console.log("Fetching data...")
+  }
+  
+  onFetchFromGitHub = () => {
+    axiosGitHubGraphQL
+      .post('', {query: GET_ORGANIZATION})
+      .then(result => console.log(result))
+  }
+  
   render() {
+    
+    const {path} = this.state;
+    
     return (
       <div className="App">
         <h1>{TITLE}</h1>
+        
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">
+          Show open issues for https://github.com/
+          </label>
+          <input id="url" type="text" onChange={this.onChange} style={{width: '300px'}} value={path}/>
+          <button type="submit">Search</button>
+        </form>
+        <hr/>
+        
+        {/*Display the results here */}
       </div>
     );
   }
